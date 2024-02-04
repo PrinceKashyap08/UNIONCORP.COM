43fdafd3e886a62cc78d9d7983ae8ba04fdab50c
Prince <vasukashyap08@gmail.com>
2024-02-04 13:51:20 +0530

    commit

---
+++ Main.js
@@ -0,0 +1,15 @@
+// hamburger 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu(){
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
+
+// Current-year 
+const year = document.querySelector(".currentYear")
+year.innerHTML = new Date().getFullYear();
