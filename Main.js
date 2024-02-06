2e7927e2f4323b9fc55c5898301068f7e090d529
Prince <vasukashyap08@gmail.com>
2024-02-06 16:06:14 +0530

    commit

--- Main.js
+++ Main.js
@@ -7,9 +7,29 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu(){
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-}
+};
 
 
 // Current-year 
 const year = document.querySelector(".currentYear")
 year.innerHTML = new Date().getFullYear();
+
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: false,
+    autoplay: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
